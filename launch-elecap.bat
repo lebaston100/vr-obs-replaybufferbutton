c2e7af1cc201562854cd0aad4a6701e5f22ddd71
lebaston100 <26111114+lebaston100@users.noreply.github.com>
2023-08-02 09:09:12 +0200

    no longer show cmd window on elecap launch

--- launch-elecap.bat
+++ launch-elecap.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /D "%~dp0"
-elecap.exe --width 230 --height 160 --title "Replay Buffer Controller" --url "file:///%~dp0controller.html"
+start /B elecap.exe --width 230 --height 160 --title "Replay Buffer Controller" --url "file:///%~dp0controller.html"
