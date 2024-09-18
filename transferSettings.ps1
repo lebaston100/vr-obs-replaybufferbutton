40b0991809f18d211e73c9581bda824a2d37474f
lebaston100 <26111114+lebaston100@users.noreply.github.com>
2024-09-18 22:38:55 +0200

    Fix setup script for now obs-websocket versions

--- transferSettings.ps1
+++ transferSettings.ps1
@@ -1,13 +1,35 @@
-﻿# get variables from obs config file
-$pw = (Select-String -Path "$env:appdata\obs-studio\global.ini" -List -Pattern "(?<=ServerPassword=)\S+").Matches[0].Value
-$port = (Select-String -Path "$env:appdata\obs-studio\global.ini" -List -Pattern "(?<=ServerPort=)\S+").Matches[0].Value
+﻿$iniFilePath = "$env:appdata\obs-studio\global.ini"
+
+try {
+    # try to get variables from obs global.ini file (old location)
+    $server_password = (Select-String -Path $iniFilePath -List -Pattern "(?<=ServerPassword=)\S+").Matches[0].Value
+    $server_port = (Select-String -Path $iniFilePath -List -Pattern "(?<=ServerPort=)\S+").Matches[0].Value
+} catch {
+    Write-Output "No configuration data found at old location"
+}
+
+# Path to the new JSON file location
+$jsonFilePath = "$env:appdata\obs-studio\plugin_config\obs-websocket\config.json"
+
+# Check if the json file exists
+if (Test-Path -Path $jsonFilePath) {
+    try {
+        # Read the file
+        $jsonContent = Get-Content -Path $jsonFilePath -Raw | ConvertFrom-Json
+        $server_port = $jsonContent.server_port
+        $server_password = $jsonContent.server_password
+        Write-Output "Read data from new location"
+    } catch {
+        Write-Output "Error reading from new json file"
+    }
+}
 
 # print out variables
-Write-Host $pw
-Write-Host $port
+Write-Host $server_password
+Write-Host $server_port
 
 # replace text in controller.html with new values
 $controller = Get-Content -Path controller.html
-$controller = $controller -creplace "YourPasswordHERE", $pw
-$controller = $controller -creplace "4455", $port
+$controller = $controller -creplace "YourPasswordHERE", $server_password
+$controller = $controller -creplace "4455", $server_port
 $controller | Set-Content -Path controller.html
